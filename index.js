a13ae4460d3abb09728ca30f9fe33ea773d703ba
Abin Binu <abinbinu1827@gmail.com>
2025-02-25 13:36:34 +0530

    task completed

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './router/userRouter.js'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+
+const port = process.env.PORT || 5000
 dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-const port = process.env.PORT || 5000
+const accessLogStream = fs.createWriteStream(path.join(process.cwd(), "access.log"), { flags: "a" });
+app.use(morgan("combined", { stream: accessLogStream }))
+
 
 
 app.use('/',userRouter)
